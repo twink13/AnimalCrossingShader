2675875be2d2fb00f548359bab592b439bef4594
twink <twink1313@me.com>
2020-04-14 12:59:44 +0800

    Add hittset and get hit uv

--- Assets/PixelMergeEffect/Demo/Script/TestCanvasPlane.cs
+++ Assets/PixelMergeEffect/Demo/Script/TestCanvasPlane.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestCanvas : MonoBehaviour
+public class TestCanvasPlane : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Camera m_Camera;
+    public LayerMask m_HitLayers;
+
+    void Awake()
     {
-        
+        m_Camera = m_Camera != null ? m_Camera : Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            Ray ray = m_Camera.ScreenPointToRay(Input.mousePosition);
+            Physics.Raycast(ray, out hit, 100f, m_HitLayers);
+
+            if (hit.collider != null)
+            {
+                Debug.Log("hit.textureCoord = " + hit.textureCoord);
+            }
+        }
     }
 }
